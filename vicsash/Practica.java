bb5c26c9e420be136f9ef2ebfe00fa2c97d4a891
Ramzi <ramziraissuni28@gmail.com>
2022-11-06 16:00:51 +0100

    agregando converiones de base y quitando prints
    He quitado algungos prints de los menus para unficarlos en uno y para
    que el codigo sea mas corto, tambien he ha agregado unos metodos para
    la conversion de base, las converiones de base que agregado son las
    siguentes: de decimal a binario, de decimal a hexadecimal y de binario
    a decimal.

--- vicsash/Practica.java
+++ vicsash/Practica.java
@@ -9,28 +9,15 @@ public class Practica{
         // The basic structure of the menu is a do while with a switch
         do{
             System.out.println("*******************\n**  Calculadora  **\n*******************");
-            System.out.println("1. Operaciones aritméticas");
-            System.out.println("2. Conversiones de base");
-            System.out.println("--------------------------");
-            System.out.println("0. Salir del programa \n\n");
-            System.out.println("Elija una opción:");
+            System.out.println("1. Operaciones aritméticas\n2. Conversiones de base\n--------------------------\n0. Salir del programa \n\nElija una opción:");
             menu=lector.nextInt();
             lector.nextLine();
             switch(menu){
             case 1 : 
                 
                     //Second sub menu for basic opertaions
-                    System.out.println("*****************************");
-                    System.out.println("** OPERACIONES ARITMÉTICAS **");
-                    System.out.println("*****************************");
-                    System.out.println("1. Sumar");
-                    System.out.println("2. Restar");
-                    System.out.println("3. Multiplicar");
-                    System.out.println("4. Dividir");
-                    System.out.println("5. Resto de la división");
-                    System.out.println("---------------------------");
-                    System.out.println("0. Volver al menú principal");
-                    System.out.println("");
+                    System.out.println("*****************************\n** OPERACIONES ARITMÉTICAS **\n*****************************");
+                    System.out.println("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Resto de la división\n-----------------------------\n0. Volver al menú principal");
                     System.out.println("Elija una opción:");
                     do{
                         second=lector.nextInt();
@@ -83,41 +70,40 @@ public class Practica{
                         System.out.println("Dato incorrecto, repitir");
                         break;
                         }
-                }while((second!=0 || second<=3));
+                }while((second!=0));
                 
             
             
             break;
             case 2 :
-                System.out.println("**************************");
-                System.out.println("** CONVERSIONES DE BASE **");
-                System.out.println("**************************");
-                System.out.println("1. De decimal a binario");
-                System.out.println("2. De decimal a hexadecimal");
-                System.out.println("3. De binario a decimal");
-                System.out.println("4. De binario a hexadecimal");
-                System.out.println("5. De hexadecimal a binario");
-                System.out.println("6. De hexadecimal a decimal");
-                System.out.println("---------------------------");
-                System.out.println("0. Volver al menú principal");
+                System.out.println("**************************\n** CONVERSIONES DE BASE **\n**************************");
                 System.out.println("");
+                System.out.println("");
+                System.out.println("1. De decimal a binario\n2. De decimal a hexadecimal\n3. De binario a decimal\n4. De binario a hexadecimal\n5. De hexadecimal a binario\n6. De hexadecimal a decimal\n---------------------------\n");
+                System.out.println("0. Volver al menú principal\n");
                 System.out.println("Elija una opción:");
                 do{
                     third=lector.nextInt();
                     lector.nextLine();
                     switch(third){
                         case 1:
-                        System.out.print("Introduce un numero entero >= 0: ");
-                        numero = lector.nextInt();
+                            System.out.print("Introduce un numero entero >= 0: ");
+                            numero = lector.nextInt();
                         
-                        System.out.print(obtenerBinario(numero));
+                            System.out.print(decimalABinario(numero));
                             
                             break;
                         case 2:
-                            System.out.println("Prueba2");
+                            System.out.print("Introduce un numero entero >= 0: ");
+                            numero = lector.nextInt();
+                        
+                            System.out.print(decimalAHexadecimal(numero));
                             break;
                         case 3: 
-                            System.out.println("Prueba3");
+                            System.out.print("Introduce un numero entero >= 0: ");
+                            numero = lector.nextInt();
+                        
+                            System.out.print(binarioADecimal(numero));
                             break;
                         case 4:
                             System.out.println("Prueba4");
@@ -144,7 +130,6 @@ public class Practica{
             
             break;
             
-
             default :
             System.out.println("Dato introducido incorrecto, introduce dato correcto");
             
@@ -153,21 +138,57 @@ public class Practica{
         lector.close();
     }
     
+    /**
+    * Convierte de decimal a binario. Solo positivos
+    *
+    * @param decimal Decimal a convertir
+     * @return numero en formato binario, -1 si el numero es negativo
+    */
+    public static long decimalABinario(int decimal) {
+        long binario = 0;
+        int digito;
+        final int DIVISOR = 2;
+        for (int i = decimal, j = 0; i > 0; i /= DIVISOR, j++) {
+            digito = i % DIVISOR;
+            binario += digito * Math.pow(10, j);
+    }
+    return binario;
+    }
     
-    public static double obtenerBinario(int numero){
-        int exp, digito;
-        double binario;
-        while(numero<0);
-    
-        exp=0;
-        binario=0;
-        while(numero!=0){
-                digito = numero % 2;            
-                binario = binario + digito * Math.pow(10, exp);   
-                exp++;
-                numero = numero/2;
-                
+    /**
+     * Convierte de decimal a Hexadecimal. Solo positivos
+     * @param decimal Decimal a convertir
+     * @return numero en formato binario, -1 si el numero es negativo
+     */
+    public static String decimalAHexadecimal(int decimal) {
+        String hexadecimal = "";
+        String caracteresHexadecimales = "0123456789abcdef";
+        while (decimal > 0) {
+            int residuo = decimal % 16;
+            hexadecimal = caracteresHexadecimales.charAt(residuo) + hexadecimal; // Agregar a la izquierda
+            decimal /= 16;
         }
-        return binario;
+        return hexadecimal;
+    }
+    /**
+ * Convierte de decimal a binario. Solo positivos
+ *
+ * @param binario Binario a convertir en formato numerico
+ * @return numero en formato decimal, -1 si no es un numero binario
+ */
+    public static int binarioADecimal(long binario) {
+ 
+        int decimal = 0;
+        int digito;
+        final long DIVISOR = 10;
+        for (long i = binario, j = 0; i > 0; i /= DIVISOR, j++) {
+            digito = (int) (i % DIVISOR);
+            if (digito != 0 && digito != 1) {
+                return -1;
+            }
+            decimal += digito * Math.pow(2, j);
+        }
+        return decimal;
+     
     }
 }
