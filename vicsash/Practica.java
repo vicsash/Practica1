1e8d8329c8734223fce801527c8fe023e2d17e85
Ramzi <ramziraissuni28@gmail.com>
2022-11-07 10:43:56 +0100

    agregando conversiones de base y valida de entrada
    he añadido tres conversiones de base que faltaban que son las de
    binario a hexadecimal, de hexadecimal a binario, de exadecimal a
    decimal. Tambien he agregado dos validadores de entrada para las
    introducciones de los numeros y digitos binarios y hexadecimales.

--- vicsash/Practica.java
+++ vicsash/Practica.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Practica{
     public static void main(String args[]){
         
-        int menu,second,third,decimal, numero ;
+        int menu,second,third,numero ;
 
         int r = 0;
         //String decimal;
@@ -77,32 +77,58 @@ public class Practica{
                     lector.nextLine();
                     switch(third){
                         case 1:
-                            System.out.print("Introduce un numero entero >= 0: ");
+                            System.out.print("Introduce un numero Decimal >= 0: ");
                             numero = lector.nextInt();
-                        
                             System.out.print(decimalABinario(numero));
                             
                             break;
                         case 2:
-                            System.out.print("Introduce un numero entero >= 0: ");
+                            System.out.print("Introduce un numero Decimal >= 0: ");
                             numero = lector.nextInt();
                         
                             System.out.print(decimalAHexadecimal(numero));
                             break;
                         case 3: 
-                            System.out.print("Introduce un numero entero >= 0: ");
+                            System.out.print("Introduce un numero binario: ");
                             numero = lector.nextInt();
-                        
-                            System.out.print(binarioADecimal(numero));
+                            if (!validarBinario(numero)){
+                                System.out.println("Número no válido");
+                                return;
+                            }
+                            int decimal = binarioADecimal(numero);
+                            System.out.print(decimal);
                             break;
                         case 4:
-                            System.out.println("Prueba4");
+                            System.out.print("Introduce un numero binario: ");
+                            numero = lector.nextInt();
+                            if (!validarBinario(numero)){
+                                System.out.println("Número no válido");
+                                return;
+                            }
+                            decimal = binarioADecimal(numero);
+                            String hexadecimal = binarioAHexadecimal(numero);
+                            System.out.print(hexadecimal);
                             break;
                         case 5:
-                            System.out.println("Prueba5");
+                            System.out.print("Introduce un numero hexadecimal: ");
+                            hexadecimal = lector.nextLine().toUpperCase().toLowerCase();
+                            if (!validarHexadecimal(hexadecimal)) {
+                                System.out.println("Número no válido");
+                                return;
+                            }
+                            decimal = hexadecimalADecimal(hexadecimal);
+                            Long binarioResultante = decimalABinario(decimal);
+                            System.out.print(binarioResultante);
                             break;
                         case 6:
-                            System.out.println("Prueba6"); 
+                            System.out.print("Introduce un numero hexadecimal: ");
+                            hexadecimal = lector.nextLine().toUpperCase().toLowerCase();
+                            if (!validarHexadecimal(hexadecimal)) {
+                                System.out.println("Número no válido");
+                                return;
+                            }
+                            decimal = hexadecimalADecimal(hexadecimal);
+                            System.out.print(decimal);
                             break;
                         case 0:
                             System.out.println("Return");
@@ -132,7 +158,7 @@ public class Practica{
     * Convierte de decimal a binario. Solo positivos
     *
     * @param decimal Decimal a convertir
-     * @return numero en formato binario, -1 si el numero es negativo
+     * @return numero en formato binario
     */
     public static long decimalABinario(int decimal) {
         long binario = 0;
@@ -148,11 +174,11 @@ public class Practica{
     /**
      * Convierte de decimal a Hexadecimal. Solo positivos
      * @param decimal Decimal a convertir
-     * @return numero en formato binario, -1 si el numero es negativo
+     * @return numero en formato hexadecimal
      */
     public static String decimalAHexadecimal(int decimal) {
         String hexadecimal = "";
-        String caracteresHexadecimales = "0123456789abcdef";
+        String caracteresHexadecimales = "0123456789ABCDEF";
         while (decimal > 0) {
             int residuo = decimal % 16;
             hexadecimal = caracteresHexadecimales.charAt(residuo) + hexadecimal; // Agregar a la izquierda
@@ -161,11 +187,11 @@ public class Practica{
         return hexadecimal;
     }
     /**
- * Convierte de decimal a binario. Solo positivos
- *
- * @param binario Binario a convertir en formato numerico
- * @return numero en formato decimal, -1 si no es un numero binario
- */
+    * Convierte de decimal a binario. Solo positivos
+    *
+    * @param binario Binario a convertir en formato numerico
+     * @return numero en formato decimal, -1 si no es un numero binario
+     */
     public static int binarioADecimal(long binario) {
  
         int decimal = 0;
@@ -181,4 +207,88 @@ public class Practica{
         return decimal;
      
     }
+    /**
+    * Pasa un número binario (en base 2) a hexadecimal (base 16).
+   * 
+   * @param binario número entero en binario
+   * @return        número inicial pasado a hexadecimal
+   */
+    public static String binarioAHexadecimal(long binario) {
+    String hexadecimal = "";
+    String digitosHexa = "0123456789ABCDEF";
+    while (binario > 0) {
+      hexadecimal = digitosHexa.charAt((int)binarioADecimal(binario % 10000)) + hexadecimal;
+      binario = binario / 10000;
+    };
+    return hexadecimal;
+  }
+   /**
+   * Pasa un número hexadecimal (en base 10) a binario (base 2).
+   * 
+   * @param hexadecimal número entero en hexadecimal
+   * @return            número inicial pasado a binario
+   */
+    public static long hexadecimalABinario(String hexadecimal) {
+    String digitosHexa = "0123456789ABCDEF";
+    long binario = 0;
+  
+    for (int i = 0; i < hexadecimal.length(); i++) {
+      final long decimal = decimalABinario(digitosHexa.indexOf(hexadecimal.charAt(i)));
+      binario = binario * 10000 + decimal;
+    }
+
+    return binario;
+    }
+    /**
+     * Pasa un numero hexadecimal a un numero decimal
+     * @param hexadecimal numero a pasar 
+     * @return numero incial pasado a decimal
+     */
+    public static int hexadecimalADecimal(String hexadecimal) {
+        String caracteresHexadecimales = "0123456789ABCDEF";
+        hexadecimal = hexadecimal.toUpperCase();
+        int decimal = 0;
+        for (int i = 0; i < hexadecimal.length(); i++) {
+            char caracter = hexadecimal.charAt(i);
+            int posicionEnCaracteres = caracteresHexadecimales.indexOf(caracter);
+            decimal = 16 * decimal + posicionEnCaracteres;
+    }
+    return decimal;
+    }
+
+    /**
+     * comprobacion del numero introduccido en binario
+     * @param binario numero a comprobar
+     * @return comprueba si esta bien introduccido o no
+     */
+    public static boolean validarBinario(int binario) {
+        // Comprobar si solo se compone de unos y ceros
+        String binarioComoCadena = String.valueOf(binario);
+        for (int i = 0; i < binarioComoCadena.length(); i++) {
+            char caracter = binarioComoCadena.charAt(i);
+            if (caracter != '0' && caracter != '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * comprobacion de lo que se intoduce es hexdecimal
+     * @param hexadecimal numero hexadecimal a comprobar
+     * @return comprueba si esta bien introduccido o no
+     */
+    public static boolean validarHexadecimal(String hexadecimal) {
+        // Comprobar si solo tiene números del 0 al 9 y letras de la A a la F
+        String caracteresHexadecimales = "0123456789ABCDEF";
+        for (int i = 0; i < hexadecimal.length(); i++) {
+            char caracter = hexadecimal.charAt(i);
+            // Si no se encuentra dentro de los caracteres válidos, regresamos false
+            if (caracteresHexadecimales.indexOf(caracter) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
+
