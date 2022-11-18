87872c775924f82359af4b34655d5ff7e336cc4c
stev02220 <102867539+stev02220@users.noreply.github.com>
2022-11-18 13:03:18 +0100

    Delete class Output

--- vicsash/Practica.java
+++ vicsash/Practica.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 public class Practica {
     public static void main(String args[]) {
 
-        int menu, second = 0, third, numero,decimal;
+        int menu, second = 0, third, numero, decimal;
         boolean repeatIfErr = false;
         String hexadecimal;
-        
-
-        int r = 0;
-        // String decimal;
 
         Scanner lector = new Scanner(System.in);
 
-        // create an object (class output) -- name: salida1 (s1)
-        Output s1 = new Output();
         // The basic structure of the menu is a do while with a switch
         do {
             System.out.println(MenuPrincipal());
@@ -24,133 +18,131 @@ public class Practica {
                 case 1:
 
                     // Second sub menu for basic opertaions
-                    
+
                     System.out.println(SubMenuAritmetica());
- 
-                    // Repeat if the user dosent input a number or inputs a higher than 5 or lower than 0
-                    // Repite si el usuario no pone un numero o pone un numero mayor que 5 o menor que 0
+
+                    // Repeat if the user dosent input a number or inputs a higher than 5 or lower
+                    // than 0
+                    // Repite si el usuario no pone un numero o pone un numero mayor que 5 o menor
+                    // que 0
                     repeatIfErr = false;
                     do {
-                        try{
+                        try {
                             // asks user for input
                             second = Integer.parseInt(lector.nextLine());
 
                             // if input too high or low, repeat input
-                            if(second > 5 || second < 0){
+                            if (second > 5 || second < 0) {
                                 System.out.println("Error: Introduce 0, 1, 2 ,3 , 4 o 5");
                                 continue;
                             }
 
-                            // if input correct, exit "do while" loop and either goes back (0) or does the operation
-                            // si el entrada era correcto, sale del "do while", si es un 0 sale al menu principaly si es otro numero menor que 6 imprime el resultado del operacion
+                            // if input correct, exit "do while" loop and either goes back (0) or does the
+                            // operation
+                            // si el entrada era correcto, sale del "do while", si es un 0 sale al menu
+                            // principaly si es otro numero menor que 6 imprime el resultado del operacion
                             repeatIfErr = true;
-                        } catch(Exception e) {
+                        } catch (Exception e) {
                             System.out.println("Error: Introduce 0, 1, 2 ,3 , 4 o 5");
-                        }  
-                    } while(repeatIfErr == false);
-                        
+                        }
+                    } while (repeatIfErr == false);
+
                     // if 0, go to main menu. else print text with result of operation
-                    if (second == 0){
+                    if (second == 0) {
                         break;
                     } else {
                         // asks user for operation numbers, prints operation result
                         // pide el usuario para los numeros del operacion, imprime el resultado
                         System.out.printf("El resultado es: %.2f \n", aritmetica(second));
-                        break; 
+                        break;
                     }
-                        
-                case 2:          
+
+                case 2:
                     do {
                         System.out.println(SubMenuConversion());
                         third = lector.nextInt();
                         lector.nextLine();
                         switch (third) {
-                            //Decimal to binary
+                            // Decimal to binary
                             case 1:
-                                do{
-                                    System.out.print("Introduce un numero Decimal >= 0: ");
-                                    numero = lector.nextInt();
-                                    System.out.print(decimalABinario(numero));
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
-                                break;
-                            //Decimal to hexadecimal    
-                            case 2:
-                                do{
-                                    System.out.print("Introduce un numero Decimal >= 0: ");
-                                    numero = lector.nextInt();
 
-                                    System.out.print(decimalAHexadecimal(numero));
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
+                                System.out.print("Introduce un numero Decimal >= 0: ");
+                                numero = lector.nextInt();
+                                System.out.print(decimalABinario(numero));
+
                                 break;
-                            //Binary to decimal    
+                            // Decimal to hexadecimal
+                            case 2:
+
+                                System.out.print("Introduce un numero Decimal >= 0: ");
+                                numero = lector.nextInt();
+
+                                System.out.print(decimalAHexadecimal(numero));
+
+                                break;
+                            // Binary to decimal
                             case 3:
-                                do{ 
-                                    System.out.print("Introduce un numero binario: ");
-                                    numero = lector.nextInt();
-                                    if (!validarBinario(numero)) {
-                                        System.out.println("Número no válido");
-                                        return;
-                                    }
-                                    decimal = binarioADecimal(numero);
-                                    System.out.print(decimal);
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
+
+                                System.out.print("Introduce un numero binario: ");
+                                numero = lector.nextInt();
+                                if (!validarBinario(numero)) {
+                                    System.out.println("Número no válido");
+                                    return;
+                                }
+                                decimal = binarioADecimal(numero);
+                                System.out.print(decimal);
+
                                 break;
-                            //Binary to hexadecimal
+                            // Binary to hexadecimal
                             case 4:
-                                do{
-                                    System.out.print("Introduce un numero binario: ");
-                                    numero = lector.nextInt();
-                                    if (!validarBinario(numero)) {
-                                        System.out.println("Número no válido");
-                                        return;
-                                    }
-                                    decimal = binarioADecimal(numero);
-                                    hexadecimal = binarioAHexadecimal(numero);
-                                    System.out.print(hexadecimal);
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
+
+                                System.out.print("Introduce un numero binario: ");
+                                numero = lector.nextInt();
+                                if (!validarBinario(numero)) {
+                                    System.out.println("Número no válido");
+                                    return;
+                                }
+                                decimal = binarioADecimal(numero);
+                                hexadecimal = binarioAHexadecimal(numero);
+                                System.out.print(hexadecimal);
+
                                 break;
-                            //Hexadecimal to binary    
+                            // Hexadecimal to binary
                             case 5:
-                                do{
-                                    System.out.print("Introduce un numero hexadecimal: ");
-                                    hexadecimal = lector.nextLine().toUpperCase();
-                                    if (!validarHexadecimal(hexadecimal)) {
-                                        System.out.println("Número no válido");
-                                        return;
-                                    }
-                                    decimal = hexadecimalADecimal(hexadecimal);
-                                    Long binarioResultante = decimalABinario(decimal);
-                                    System.out.print(binarioResultante);
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
+
+                                System.out.print("Introduce un numero hexadecimal: ");
+                                hexadecimal = lector.nextLine().toUpperCase();
+                                if (!validarHexadecimal(hexadecimal)) {
+                                    System.out.println("Número no válido");
+                                    return;
+                                }
+                                decimal = hexadecimalADecimal(hexadecimal);
+                                Long binarioResultante = decimalABinario(decimal);
+                                System.out.print(binarioResultante);
+
                                 break;
-                            //Hexadecimal to decimal    
+                            // Hexadecimal to decimal
                             case 6:
-                                do{
-                                    System.out.print("Introduce un numero hexadecimal: ");
-                                    hexadecimal = lector.nextLine().toUpperCase();
-                                    if (!validarHexadecimal(hexadecimal)) {
-                                        System.out.println("Número no válido");
-                                        return;
-                                    }
-                                    decimal = hexadecimalADecimal(hexadecimal);
-                                    System.out.print(decimal);
-                                    s1.continues();
-                                }while (s1.continuar.equals("si"));
+
+                                System.out.print("Introduce un numero hexadecimal: ");
+                                hexadecimal = lector.nextLine().toUpperCase();
+                                if (!validarHexadecimal(hexadecimal)) {
+                                    System.out.println("Número no válido");
+                                    return;
+                                }
+                                decimal = hexadecimalADecimal(hexadecimal);
+                                System.out.print(decimal);
+
                                 break;
                             default:
-                            if(third > 6 || 0 < third){
-                                System.out.println("Dato incorrecto, repetir");
-                                break;
-                            }
+                                if (third > 6 || 0 < third) {
+                                    System.out.println("Dato incorrecto, repetir");
+                                    break;
+                                }
                         }
 
                     } while ((third != 0));
-                    
+
                     break;
                 case 0:
                     System.out.println("Fin");
@@ -172,22 +164,25 @@ public class Practica {
      * 
      * Usa un numero de 1 a 5 para decidir que operacion hay que usar,
      * imprime dos veces para la entrada de datos
+     * 
      * @param op
      * @return
      */
-    public static double aritmetica(int op){
+    public static double aritmetica(int op) {
         double result = 0, num1 = 0, num2 = 0;
 
-        // Closing the scanner inside the method closes "System.in" meaning scanners outside of the method will fail
-        // Cerrando un scanner dentro de un metodo cierra "System.in", significa que el scanner dejara de funcionar dentro del programa
+        // Closing the scanner inside the method closes "System.in" meaning scanners
+        // outside of the method will fail
+        // Cerrando un scanner dentro de un metodo cierra "System.in", significa que el
+        // scanner dejara de funcionar dentro del programa
         Scanner userInput = new Scanner(System.in);
         boolean noError = false;
 
         // Output and input for "num1"
-        do{
-            try{
+        do {
+            try {
                 System.out.println("Introduce el primer numero: ");
-                num1 = Double.parseDouble(userInput.nextLine()); 
+                num1 = Double.parseDouble(userInput.nextLine());
                 noError = true;
             } catch (Exception e) {
                 System.out.println("Habia un error. Intente de vuelta ");
@@ -197,17 +192,17 @@ public class Practica {
         noError = false;
 
         // Output and input for "num2"
-        do{
-            try{
+        do {
+            try {
                 System.out.println("Introduce el segundo numero: ");
-                num2 = Double.parseDouble(userInput.nextLine()); 
+                num2 = Double.parseDouble(userInput.nextLine());
                 noError = true;
             } catch (Exception e) {
                 System.out.println("Habia un error. Intente de vuelta ");
             }
         } while (!noError);
 
-        switch (op){
+        switch (op) {
             case 1:
                 // Addition
                 result = num1 + num2;
@@ -230,13 +225,12 @@ public class Practica {
                 break;
             default:
                 System.out.println("Error: Introduce 0, 1, 2 ,3 , 4 o 5");
+                userInput.close();
         }
 
         return result;
-        
+
     }
-
-
 
     /**
      * Convierte de decimal a binario. Solo positivos
@@ -267,7 +261,7 @@ public class Practica {
         while (decimal > 0) {
             int residuo = decimal % 16;
             // Agregar a la izquierda
-            hexadecimal = caracteresHexadecimales.charAt(residuo) + hexadecimal; 
+            hexadecimal = caracteresHexadecimales.charAt(residuo) + hexadecimal;
             decimal /= 16;
         }
         return hexadecimal;
@@ -384,43 +378,49 @@ public class Practica {
         }
         return true;
     }
+
     /**
      * Method for printing out the main menu
+     * 
      * @return the String menu which represents the main menu
      */
-    public static String MenuPrincipal(){
+    public static String MenuPrincipal() {
         String Calculadora = "*******************\n**  Calculadora  **\n*******************";
         String MenuPrincipal = "\n1. Operaciones aritméticas\n2. Conversiones de base";
         String Line = "\n--------------------------";
-        String  ExitChoice = "\n0.Salir del programa \n Elija una opción:";
+        String ExitChoice = "\n0.Salir del programa \n Elija una opción:";
         String menu = Calculadora + MenuPrincipal + Line + ExitChoice;
-        
-    return  menu;
+
+        return menu;
     }
+
     /**
      * Method to print out the submenu for basic operations
+     * 
      * @return submenu string to print it out
      */
-    public static String SubMenuAritmetica(){
+    public static String SubMenuAritmetica() {
         String OperacionAritmetica = "*****************************\n** OPERACIONES ARITMÉTICAS **\n*****************************";
         String SubMenuAritmetica = "\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Resto de la división";
         String Line = "\n--------------------------";
-        String  ExitChoice = "\n0.Salir del programa \n Elija una opción:";
+        String ExitChoice = "\n0.Salir del programa \n Elija una opción:";
         String submenu = OperacionAritmetica + SubMenuAritmetica + Line + ExitChoice;
-       
-        return  submenu;
+
+        return submenu;
     }
+
     /**
      * Method to print out the submenu for conversions
+     * 
      * @return submenu string to print it out
      */
-    public static String SubMenuConversion(){
+    public static String SubMenuConversion() {
         String ConversionBase = "**************************\n** CONVERSIONES DE BASE **\n**************************";
         String Space = "\n";
         String SubMenuConversionp1 = "1. De decimal a binario\n2. De decimal a hexadecimal\n3. De binario a decimal";
         String SubMenuConversionp2 = "\n4. De binario a hexadecimal\n5. De hexadecimal a binario\n6. De hexadecimal a decimal";
         String Line = "\n--------------------------";
-        String  ExitChoice = "\n0.Salir del programa \n Elija una opción:";
+        String ExitChoice = "\n0.Salir del programa \n Elija una opción:";
         String submenu = ConversionBase + Space + SubMenuConversionp1 + SubMenuConversionp2 + Line + ExitChoice;
         return submenu;
     }
